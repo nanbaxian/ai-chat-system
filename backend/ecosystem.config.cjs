af48582060c7f38e004a1309d0ccc799d596aa3f
nanbaxian <nanbaxian@gmail.com>
2026-01-01 08:48:57 -0500

    fix pm error

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   apps: [{
     name: "webrtc-signal",
-    script: "webrtc-service/src/server.js",
+    script: "webrtc-service/src/server.ts",
+    interpreter: "./node_modules/.bin/tsx",
     env: {
       HOST: "127.0.0.1",
       PORT: 3001
