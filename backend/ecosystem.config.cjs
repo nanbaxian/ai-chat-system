79dedfc99af6821007507f7ee1e76501e7a09222
nanbaxian <nanbaxian@gmail.com>
2026-01-01 08:46:07 -0500

    change path

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "webrtc-signal",
-    script: "server.js",
+    script: "webrtc-service/src/server.js",
     env: {
       HOST: "127.0.0.1",
       PORT: 3001
