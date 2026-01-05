20a65ede22be563e176e88ab725e94b7a763b6c7
nanbaxian <nanbaxian@gmail.com>
2026-01-04 21:28:32 -0500

    Revert "fix cors"
    This reverts commit 47f4ed38661fb844bc5a690bd46501fda63fd754.

--- backend/webrtc-service/src/server.ts
+++ backend/webrtc-service/src/server.ts
@@ -377,7 +377,7 @@ const corsOptions = {
 };
 const app = express();
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions), (_req, res) => res.sendStatus(204));
+app.options('/signal', cors(corsOptions));
 app.use(express.json({ limit: '6mb' }));
 
 app.post('/signal', async (req, res) => {
